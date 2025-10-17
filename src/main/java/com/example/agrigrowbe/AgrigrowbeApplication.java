d067683768c1a33351d4adf2c912dca829d23c10
jyotsnatalasila <jyotsnatalasila@gmail.com>
2025-10-17 14:26:37 +0530

    4rth Commit

--- src/main/java/com/example/agrigrowbe/AgrigrowbeApplication.java
+++ src/main/java/com/example/agrigrowbe/AgrigrowbeApplication.java
@@ -2,12 +2,18 @@ package com.example.agrigrowbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AgrigrowbeApplication {
+public class AgrigrowbeApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgrigrowbeApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AgrigrowbeApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(AgrigrowbeApplication.class, args);
+    }
 }

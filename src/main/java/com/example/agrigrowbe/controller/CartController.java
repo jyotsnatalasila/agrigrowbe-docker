ff230a945bd890dc45a721afe835103e4c5f8fb5
jyotsnatalasila <jyotsnatalasila@gmail.com>
2025-11-28 00:29:59 +0530

    Update CORS configuration for CartController

--- src/main/java/com/example/agrigrowbe/controller/CartController.java
+++ src/main/java/com/example/agrigrowbe/controller/CartController.java
@@ -10,15 +10,10 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/cart")
-@CrossOrigin(origins = {"http://const res = await axios.post(
-  `http://ec2-13-62-223-118.eu-north-1.compute.amazonaws.com:1010/agrigrowbe/api/password/reset?token=${encodeURIComponent(token)}`,
-  { password: newPassword }
-);
-:4000", "http://const res = await axios.post(
-  `http://ec2-13-62-223-118.eu-north-1.compute.amazonaws.com:1010/agrigrowbe/api/password/reset?token=${encodeURIComponent(token)}`,
-  { password: newPassword }
-);
-:8080"}) // ✅ UPDATED
+@CrossOrigin(origins = {
+    "http://ec2-13-62-223-118.eu-north-1.compute.amazonaws.com:4000",
+    "http://ec2-13-62-223-118.eu-north-1.compute.amazonaws.com:8080"
+})
 public class CartController {
 
     private final CartItemRepository cartItemRepository;
